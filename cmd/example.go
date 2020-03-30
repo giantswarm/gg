45731de3d2624fd1b6c878dac16c338823699fb1
Tim Schindler <xh3b4sd@gmail.com>
2020-03-30 15:08:55 +0200

    update readme

--- cmd/example.go
+++ cmd/example.go
@@ -1,20 +1,22 @@
 package cmd
 
-var example = `    The following examples make use of a test.json file which contains JSON logs
-    where each log line is a single JSON object.
+var example = `    The following examples make use of a basic.json file which contains JSON
+    logs where each log line is a single JSON object. You can find the
+    basic.json file in ./cmd/fixture along with other test fixtures used for
+    golden file tests.
 
     Select all logs where any key matches "obj" and its associated value matches
     "qihx8". This can be used to e.g. grep for all logs related to Tenant
     Cluster "qihx8".
 
-        cat test.json | gg -s obj:qihx8
+        cat basic.json | gg -s obj:qihx8
 
     Select all logs like the example above but on top of that filter also for
     logs where any key matches "res" and its associated value matches "dra".
     This can be used to e.g. grep for all logs of the "drainer" and
     "drainfinisher" resource implementation.
 
-        cat test.json | gg -s obj:qihx8 -s res:dra
+        cat basic.json | gg -s obj:qihx8 -s res:dra
 
     Select all logs like the example above but on top of that only output
     key-value pairs of the logs where any key matches "ti" or "mes". This can be
@@ -23,15 +25,15 @@ var example = `    The following examples make use of a test.json file which con
     the output since "time" is always consistently formatted, whereas "message"
     can be of almost arbitrary length.
 
-        cat test.json | gg -s obj:qihx8 -s res:dra -f ti,mes
+        cat basic.json | gg -s obj:qihx8 -s res:dra -f ti,mes
 
     Select all logs like the example above but on top of that group output
     key-value pairs of the logs based on the common value of associated keys
     matching "lo". This can be used to e.g. group resource logs by their
     reconciliation "loop".
 
-        cat test.json | gg -s obj:qihx8 -s res:dra -f ti,mes -g lo
+        cat basic.json | gg -s obj:qihx8 -s res:dra -f ti,mes -g lo
 
     Display the list of resources executed for a given CR.
 
-        cat test.json | gg -s obj:qihx8 -s con:mac -f res -o text -g lo | uniq`
+        cat basic.json | gg -s obj:qihx8 -s con:mac -f res -o text -g lo | uniq`
